5606d05d547f1af248bc1c82fc3c627d70bd9585
Adrien Hiribarren Touya <adrien64250@yahoo.com>
2024-11-14 14:36:27 +0100

    ✨ Fichier d'initalisation de la db

---
+++ src/back/db/scripts/init.sql
@@ -0,0 +1,41 @@
+CREATE TABLE IF NOT EXISTS Utilisateur (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    mail VARCHAR(255) NOT NULL,
+    mdpHash VARCHAR(255) NOT NULL,
+    dateInscription DATE NOT NULL,
+    theme INT NOT NULL,
+    urlPfp VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Dossier (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(255) NOT NULL,
+    idParent INT,
+    FOREIGN KEY (idParent) REFERENCES Dossier(id)
+);
+
+CREATE TABLE IF NOT EXISTS Algorithme (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(255) NOT NULL,
+    url VARCHAR(255) NOT NULL,
+    dateCreation DATE NOT NULL,
+    dateModification DATE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS PermDossier (
+    idUtilisateur INT NOT NULL,
+    idDossier INT NOT NULL,
+    droits VARCHAR(255) NOT NULL,
+    PRIMARY KEY (idUtilisateur, idDossier),
+    FOREIGN KEY (idUtilisateur) REFERENCES Utilisateur(id),
+    FOREIGN KEY (idDossier) REFERENCES Dossier(id)
+);
+
+CREATE TABLE IF NOT EXISTS PermAlgorithme (
+    idUtilisateur INT NOT NULL,
+    idAlgorithme INT NOT NULL,
+    droits VARCHAR(255) NOT NULL,
+    PRIMARY KEY (idUtilisateur, idAlgorithme),
+    FOREIGN KEY (idUtilisateur) REFERENCES Utilisateur(id),
+    FOREIGN KEY (idAlgorithme) REFERENCES Algorithme(id)
+);
