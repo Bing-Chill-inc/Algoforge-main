8fd3f8abfa787f6d250405f16fdfab97cb96692a
ToxykAuBleu <foissac.mathieu@gmail.com>
2024-11-19 16:15:01 +0100

    🔧 Bug fix: mauvaise utilisation de l'option "host".

--- src/back/db/data-source.ts
+++ src/back/db/data-source.ts
@@ -2,10 +2,9 @@ import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { User } from "./schemas/User.schema";
 
-console.log("ENV: ", process.env);
 export const AppDataSource = new DataSource({
 	type: "postgres",
-	host: "localhost",
+	host: "db_postgres",
 	port: Number(process.env.POSTGRES_PORT),
 	username: process.env.POSTGRES_USER,
 	password: process.env.POSTGRES_PASSWORD,
