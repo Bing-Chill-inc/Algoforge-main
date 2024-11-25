69efb5c4aa5a7700c3ddd2057ce935747a1a5549
ToxykAuBleu <foissac.mathieu@gmail.com>
2024-11-25 11:48:06 +0100

    🔧 Mise en place du lien avec le front cloud.

--- src/back/index.ts
+++ src/back/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import path from "path";
 import type RouteHandler from "./types/RouteHandler";
 
@@ -9,6 +10,7 @@ import { iconHandler } from "./getBibliotheques";
 
 const app = express();
 const port = 3000;
+app.use(cors());
 
 AssetsDynamiques.forEach((asset: RouteHandler) => {
 	app.get(`/edit/assetsDynamiques${asset.route}`, asset.callback);
@@ -18,19 +20,22 @@ app.get(getBibliothèque.route, getBibliothèque.callback);
 
 app.get(iconHandler.route, iconHandler.callback);
 
-app.use("/edit", express.static(path.join(__dirname, "../front-editeur/src")));
+app.use("/edit", express.static(path.join(__dirname, "/../front-editeur/src")));
+app.use("/cloud", express.static(path.join(__dirname, "/../front-cloud/dist")));
 
 // Init database connection
 import { AppDataSource } from "./db/data-source";
 
-AppDataSource.initialize().then(async () => {
-	console.log("Database connection initialized");
+AppDataSource.initialize()
+	.then(async () => {
+		console.log("Database connection initialized");
 
-	// Start server
-	app.listen(port, () => {
-		console.log(`Server is running on http://localhost:${port}`);
+		// Start server
+		app.listen(port, () => {
+			console.log(`Server is running on http://localhost:${port}`);
+		});
+	})
+	.catch((err) => {
+		console.error("Error while initializing database connection: \n", err);
+		process.exit(1);
 	});
-}).catch((err) => {
-	console.error("Error while initializing database connection: \n", err);
-	process.exit(1);
-});
