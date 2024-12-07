c4e7198f21159144cca68408b9de98ebac761233
ToxykAuBleu <foissac.mathieu@gmail.com>
2024-12-07 21:49:09 +0100

    ⚙️ Amélioration du middleware d'erreur: il prend désormais en compte le build de l'application pour afficher ou non les détails des erreurs.

--- src/back/middlewares/error.middleware.ts
+++ src/back/middlewares/error.middleware.ts
@@ -5,11 +5,17 @@ import { Logger } from "../utils/logger";
  * Il sert également à ce que l'application ne plante pas en cas d'erreur.
  */
 export const errorMiddleware = async (error, req, res, next) => {
+	const errorMessage: string =
+		process.env.BUILD === "dev" ? error.stack : error.message;
+
 	Logger.error(
-		`Une rreur est survenue lors du traitement de la requête: \n${error.stack}`,
+		`Une rreur est survenue lors du traitement de la requête: \n${errorMessage}`,
 		"middleware: error",
 	);
-	res.status(500).json({
+
+	const statusCode = error.statusCode || 500;
+	res.status(statusCode).json({
+		statusCode: statusCode,
 		message: "Une erreur est survenue lors du traitement de la requête.",
 	});
 };
