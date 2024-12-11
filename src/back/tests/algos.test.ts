893f26a84ea5b1f6ba8c362b30bed10d40b6da1e
ToxykAuBleu <foissac.mathieu@gmail.com>
2024-12-11 22:46:29 +0100

    ✨ Ajout de la validation des algorithmes. ⚙️ Implémentation de quelques tests unitaires pour la vérification d'algorithmes. (Les algorithmes de références seront sûrement à revoir.) (Le prochain commit planifiera les autres tests à implémenter pour couvrir tous les cas de figure.)

--- src/back/tests/algos.test.ts
+++ src/back/tests/algos.test.ts
@@ -2,9 +2,16 @@
 // Il faudra intégrer des tests intermédiaires, plus précisément avec des vérifications des données envoyées.
 import { Logger } from "../utils/logger";
 import { server, request } from "./setup";
+import { AlgoValidator } from "../utils/algoValidator";
+import { readFileSync } from "fs";
 
 import { describe, expect, test } from "bun:test";
+import path from "path";
 
+/**
+ * Tests des routes de l'API des algorithmes.
+ * Nous considérons que la base de données est vide.
+ */
 describe("Algos: empty database", () => {
 	test("GET /api/algos/byUserId/:id -> Aucun algorithme trouvé.", async () => {
 		const response = await request.get("/api/algos/byUserId/1");
@@ -41,3 +48,70 @@ describe("Algos: empty database", () => {
 		expect(response.body).toHaveProperty("message");
 	});
 });
+
+/**
+ * Tests du service de la validation des algorithmes.
+ * Nous considérons que les algorithmes en input de test sont des objets json valides.
+ * Ce qui nous intéresse ici est de vérifier que l'algorithme est bien conforme à la structure attendue.
+ */
+describe("Algos: Validator", () => {
+	test("Algo référence n°1. -> aucune erreur.", async () => {
+		// Validation de l'algorithme.
+		const result = validationAlgo("algo-1");
+		// Vérification du résultat.
+		expect(result).toHaveProperty("success", true);
+	});
+	test("Algo référence n°2. -> manquant: listeDonnes, listeResultats.", async () => {
+		// Validation de l'algorithme.
+		const result = validationAlgo("algo-2");
+		// Vérification du résultat.
+		expect(result).toHaveProperty("success", true);
+	});
+	test("Algo référence n°3. -> 8 erreurs: mauvais format de abscisse/ordonnee.", async () => {
+		// Validation de l'algorithme.
+		const result = validationAlgo("algo-3");
+		// Vérification du résultat.
+		expect(result.error.issues).toBeArrayOfSize(8);
+	});
+	test("Algo référence n°10. -> aucune erreur.", async () => {
+		// Validation de l'algorithme.
+		const result = validationAlgo("algo-complet");
+		// Vérification du résultat.
+		expect(result).toHaveProperty("success", true);
+	});
+});
+
+/**
+ * Valide un algorithme.
+ * @param algoName Nom/chemin de l'algorithme.
+ * @returns Objet contenant le résultat de la validation.
+ * @example
+ * const result = validationAlgo("algo-1");
+ * // => { success: true, data: [{...}] }
+ */
+function validationAlgo(algoName: string) {
+	let result;
+	try {
+		// Récupération de l'algorithme de référence.
+		const algo = readAlgo(algoName);
+		// Validation de l'algorithme.
+		result = AlgoValidator.validateAlgo(algo);
+	} catch (error) {
+		Logger.error(`${error.stack}`, "test: algos");
+	}
+	return result;
+}
+
+/**
+ * Lit un algorithme depuis un fichier json.
+ * @param algoName Nom/chemin de l'algorithme.
+ * @returns L'algorithme sous forme d'objet json.
+ * @example
+ * const algo = readAlgo("algo-1");
+ * // => [{ typeElement: "Probleme", ... }]
+ */
+function readAlgo(algoName: string) {
+	return JSON.parse(
+		readFileSync(path.join(__dirname, `./json/${algoName}.json`), "utf-8"),
+	);
+}
---
+++ src/back/utils/algoValidator.ts
@@ -0,0 +1,122 @@
+import { z } from "zod";
+import { Logger } from "./logger";
+
+// Définition des types d'éléments possibles.
+enum TypeElement {
+	Probleme = "Probleme",
+	StructureIterativeNonBornee = "StructureIterativeNonBornee",
+	StructureSi = "StructureSi",
+	Condition = "Condition",
+	ConditionSortie = "ConditionSortie",
+	DictionnaireDonnee = "DictionnaireDonnee",
+}
+const TypeElementEnum = z.enum([
+	TypeElement.Probleme,
+	TypeElement.StructureIterativeNonBornee,
+	TypeElement.StructureSi,
+	TypeElement.Condition,
+	TypeElement.ConditionSortie,
+	TypeElement.DictionnaireDonnee,
+]);
+
+// Définition des schémas de validation.
+const coordonneeSchema = z.string().endsWith("vw");
+const enfants = z.array(
+	z.lazy(() =>
+		z.discriminatedUnion("typeElement", [
+			ProblemeSchema,
+			StructureSiSchema,
+			StructureIterativeNonBorneeSchema,
+			ConditionSortieSchema,
+			DictionnaireDonneeSchema,
+		]),
+	),
+);
+
+// -> Probleme
+const BaseProblemeSchema = z.object({
+	typeElement: TypeElementEnum.extract([TypeElement.Probleme]),
+	abscisse: coordonneeSchema,
+	ordonnee: coordonneeSchema,
+	libelle: z.string(),
+	listeDonnes: z.array(z.string()).default([""]),
+	listeResultats: z.array(z.string()).default([""]),
+});
+const ProblemeSchema = BaseProblemeSchema.extend({
+	enfants: enfants.default([]),
+});
+
+// -> Dictionnaire de données
+// TODO: compléter avec les valeurs possibles du dictionnaire de données.
+const DictionnaireDonneeSchema = z.object({
+	typeElement: TypeElementEnum.extract([TypeElement.DictionnaireDonnee]),
+});
+
+// -> Structure itérative non bornée
+const BaseStructureIterativeNonBorneeSchema = z.object({
+	typeElement: TypeElementEnum.extract([
+		TypeElement.StructureIterativeNonBornee,
+	]),
+	abscisse: coordonneeSchema,
+	ordonnee: coordonneeSchema,
+});
+const StructureIterativeNonBorneeSchema =
+	BaseStructureIterativeNonBorneeSchema.extend({
+		enfants: enfants.default([]),
+	});
+
+// TODO: -> Structure iterative bornée
+
+// -> Condition
+const BaseConditionSchema = z.object({
+	typeElement: TypeElementEnum.extract([TypeElement.Condition]),
+	libelle: z.string(),
+});
+const ConditionSchema = BaseConditionSchema.extend({
+	enfants: enfants.default([]),
+});
+// -> Structure si
+const StructureSiSchema = z.object({
+	typeElement: TypeElementEnum.extract([TypeElement.StructureSi]),
+	abscisse: coordonneeSchema,
+	ordonnee: coordonneeSchema,
+	conditions: z.array(ConditionSchema).default([]),
+});
+
+// -> Condition de sortie
+const ConditionSortieSchema = z.object({
+	typeElement: TypeElementEnum.extract([TypeElement.ConditionSortie]),
+	abscisse: coordonneeSchema,
+	ordonnee: coordonneeSchema,
+});
+
+// === Algorithme
+const AlgoSchema = enfants;
+
+/**
+ * Classe de validation des algorithmes.
+ */
+export class AlgoValidator {
+	constructor() {
+		throw new Error("Cette classe n'est pas instanciable.");
+	}
+
+	/**
+	 * Valide un algorithme.
+	 * @param algo Algorithme à valider.
+	 * @returns Objet contenant le résultat de la validation.
+	 * @example
+	 * // Types de retour possibles:
+	 * // { success: true, data: [{...}] }
+	 * // { success: false, error: [{...}] }
+	 */
+	static validateAlgo(algo: Object) {
+		try {
+			const parsedAlgo = AlgoSchema.safeParse(algo);
+			Logger.debug(JSON.stringify(parsedAlgo), "AlgoValidator", 6);
+			return parsedAlgo;
+		} catch (error) {
+			throw error;
+		}
+	}
+}
