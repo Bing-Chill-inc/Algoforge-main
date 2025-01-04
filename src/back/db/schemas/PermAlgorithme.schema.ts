41f7e4534456a899f68903d3b7777b3be00cfdf9
ToxykAuBleu <foissac.mathieu@gmail.com>
2025-01-04 18:46:47 +0100

    🔧 Bug fix: PermAlgorithme & PermDossier, nom des clés mal définies.

--- src/back/db/schemas/PermAlgorithme.schema.ts
+++ src/back/db/schemas/PermAlgorithme.schema.ts
@@ -1,22 +1,23 @@
-import { Entity, Column, PrimaryColumn, ManyToOne } from "typeorm";
+import { Entity, Column, PrimaryColumn, ManyToOne, JoinColumn } from "typeorm";
 import type { Relation } from "typeorm";
 import { Utilisateur } from "./Utilisateur.schema";
 import { Algorithme } from "./Algorithme.schema";
-import { Dossier } from "./Dossier.schema";
 
 @Entity()
 export class PermAlgorithme {
-	@PrimaryColumn({ type: "int" })
+	@PrimaryColumn({ type: "int", name: "idUtilisateur" })
 	idUtilisateur: number;
 
-	@PrimaryColumn({ type: "int" })
+	@PrimaryColumn({ type: "int", name: "idAlgorithme" })
 	idAlgorithme: number;
 
 	@Column({ type: "varchar", length: 255 })
 	droits: string;
 
 	@ManyToOne(() => Utilisateur, (utilisateur) => utilisateur.permAlgorithmes)
+	@JoinColumn({ name: "idUtilisateur" })
 	utilisateur: Relation<Utilisateur>;
 	@ManyToOne(() => Algorithme, (algorithme) => algorithme.permAlgorithmes)
+	@JoinColumn({ name: "idAlgorithme" })
 	algorithme: Relation<Algorithme>;
 }
--- src/back/db/schemas/PermDossier.schema.ts
+++ src/back/db/schemas/PermDossier.schema.ts
@@ -1,21 +1,23 @@
-import { Entity, Column, PrimaryColumn, ManyToOne } from "typeorm";
+import { Entity, Column, PrimaryColumn, ManyToOne, JoinColumn } from "typeorm";
 import type { Relation } from "typeorm";
 import { Utilisateur } from "./Utilisateur.schema";
 import { Dossier } from "./Dossier.schema";
 
 @Entity()
 export class PermDossier {
-	@PrimaryColumn({ type: "int" })
+	@PrimaryColumn({ type: "int", name: "idUtilisateur" })
 	idUtilisateur: number;
 
-	@PrimaryColumn({ type: "int" })
+	@PrimaryColumn({ type: "int", name: "idDossier" })
 	idDossier: number;
 
 	@Column({ type: "varchar", length: 255 })
 	droits: string;
 
 	@ManyToOne(() => Utilisateur, (utilisateur) => utilisateur.permDossiers)
+	@JoinColumn({ name: "idUtilisateur" })
 	utilisateur: Relation<Utilisateur>;
 	@ManyToOne(() => Dossier, (dossier) => dossier.permDossiers)
+	@JoinColumn({ name: "idDossier" })
 	dossier: Relation<Dossier>;
 }
