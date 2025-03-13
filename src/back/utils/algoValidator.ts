052d24f8e1c6e24b0eaec2044e6bcb8e14ca267e
Feror <contact@feror.fr>
2025-03-13 16:43:12 +0100

    Ajout du validator du dictionnaire de données et de 3 tests pour tester un dictionnaire vide, un dictionnaire valide et un dictionnaire faux

--- src/back/utils/algoValidator.ts
+++ src/back/utils/algoValidator.ts
@@ -62,6 +62,8 @@ const ProblemeSchema = BaseProblemeSchema.extend({
 // TODO: compléter avec les valeurs possibles du dictionnaire de données.
 const DictionnaireDonneeSchema = z.object({
 	typeElement: TypeElementEnum.extract([TypeElement.DictionnaireDonnee]),
+	types: z.record(z.string()).optional(),
+	signification: z.record(z.string()).optional(),
 });
 
 // -> Structure itérative non bornée
